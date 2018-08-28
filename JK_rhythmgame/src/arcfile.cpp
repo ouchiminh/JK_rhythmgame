baecd7ffaa49f7e6ec2c75521eaefec4ce91cd53
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2018-08-28 17:29:18 +0900

    add archiver elm

---
+++ JK_rhythmgame/src/arcfile.cpp
@@ -0,0 +1,39 @@
+#include "arcfile.hpp"
+
+jk::archive::file::file() noexcept : body_{ nullptr }, size_{ 0 } {}
+
+jk::archive::file::~file() {
+	if (body_) delete[] body_;
+}
+
+bool jk::archive::file::is_avail() const noexcept {
+	return body_ == nullptr ||
+		size_ == 0 ||
+		filepath_.empty() ?
+		false :
+		true;
+}
+
+void * jk::archive::file::get_body() noexcept {
+	return body_;
+}
+
+void const * jk::archive::file::get_body() const noexcept {
+	return body_;
+}
+
+size_t jk::archive::file::size() const noexcept {
+	return size_;
+}
+
+bool jk::archive::file::operator==(const file & f, const std::filesystem::path & p) noexcept {
+	namespace fs = std::filesystem;
+	const auto cur = fs::current_path();
+	return f.filepath_.lexically_relative(cur) == p.lexically_relative(cur);
+}
+
+bool jk::archive::file::operator==(const std::filesystem::path & p, const file & f) noexcept {
+	namespace fs = std::filesystem;
+	const auto cur = fs::current_path();
+	return f.filepath_.lexically_relative(cur) == p.lexically_relative(cur);
+}
--- JK_rhythmgame/src/arcfile.hpp
+++ JK_rhythmgame/src/arcfile.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdexcept>
 #include <filesystem>
 #include <string>
 #include <cstddef>
@@ -10,11 +11,21 @@ namespace jk::archive {
 		size_t size_;	// if invalid, 0
 		std::filesystem::path filepath_;
 	public:
+		/// <summary>
+		/// <para>constructor</para>
+		/// load from normal file.
+		/// </summary>
 		template<class IStream>
-		file(const std::filesystem::path & filepath, IStream & in, size_t size) throw(std::runtime_error);
+		file(const std::filesystem::path & filepath, IStream & in, size_t filesize) throw(std::runtime_error);
 		template<class IStream>
-		file(std::filesystem::path && filepath, IStream & in, size_t size) throw(std::runtime_error);
+		file(std::filesystem::path && filepath, IStream & in, size_t filesize) throw(std::runtime_error);
 		file() noexcept;			// initialize as invalid object
+		~file();
+
+		template<class IStream>
+		void load_from_archive(const std::filesystem::path & filepath, IStream & in, size_t filesize);
+		template<class IStream>
+		void load_from_file(const std::filesystem::path & filepath, IStream & in, size_t filesize);
 
 		bool is_avail() const noexcept;
 		void * get_body() noexcept;
@@ -22,10 +33,61 @@ namespace jk::archive {
 		size_t size() const noexcept;
 
 		template<class OStream>
-		void write(OStream & out);
+		void write_header(OStream & out) throw(std::runtime_error);
+		template<class OStream>
+		void write_body(OStream & out) throw(std::runtime_error);
 
-		file & operator=(const std::filesystem::path & p);
 		static bool operator==(const file & f, const std::filesystem::path & p) noexcept;
 		static bool operator==(const std::filesystem::path & p, const file & f) noexcept;
 	};
+
+	template<class IStream>
+	inline file::file(const std::filesystem::path & filepath, IStream & in, size_t filesize) throw(std::runtime_error) {
+		load_from_file(filepath, in, filesize);
+	}
+
+	template<class IStream>
+	inline file::file(std::filesystem::path && filepath, IStream & in, size_t filesize) throw(std::runtime_error) {
+		load_from_file(filepath, in, filesize);
+	}
+	template<class IStream>
+	inline void file::load_from_archive(const std::filesystem::path & filepath, IStream & in, size_t filesize) {
+		using is = istream_traits<IStream>;
+		filepath_ = filepath;
+		size_ = filesize;
+		body_ = new std::int8_t[filesize];
+		is::read(in, body_, filesize);
+	}
+	template<class IStream>
+	inline void file::load_from_file(const std::filesystem::path & filepath, IStream & in, size_t filesize) {
+	using is = jk::istream_traits<IStream>;
+		namespace fs = std::filesystem;
+		if(!fs::exists(filepath)) throw std::runtime_error("file not found.");
+		try {
+			body_ = new std::int8_t[filesize];
+			is::read(in, body_, filesize);
+		} catch (std::bad_alloc & e) {
+			throw std::runtime_error("allocation failed");
+		} catch (std::exception & e) { throw std::runtime_error(e.what()); }
+		size_ = filesize;
+		filepath_ = filepath;
+	}
+
+	template<class OStream>
+	inline void file::write_header(OStream & out) throw(std::runtime_error) {
+		namespace fs = std::filesystem;
+		using is = istream_traits<OStream>;
+		if(!is_avail()) throw(std::runtime_error("invalid data"))
+		auto relative_path = filepath_.lexically_relative(fs::current_path());
+		is::write(out, relative_path.c_str(), relative_path.string().size());
+		is::write(out, &size_, sizeof(size));
+	}
+
+	template<class OStream>
+	inline void file::write_body(OStream & out) throw(std::runtime_error) {
+		using is = istream_traits<OStream>;
+		if (!is_avail()) throw std::runtime_error("invalid data");
+		is::write(out, body_, size_);
+	}
+
 }
