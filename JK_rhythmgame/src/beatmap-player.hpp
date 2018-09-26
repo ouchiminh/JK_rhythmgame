feda5974fdf754f4bc88addb567944e1b213d197
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2018-09-26 22:21:05 +0900

    プレイ画面作ろうな^^

---
+++ JK_rhythmgame/src/beatmap-player.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "SFML/System/Time.hpp"
+#include "ui-component.hpp"
+#include "event-handlers.hpp"
+#include "scene.hpp"
+#include "beatmap.hpp"
+
+namespace jk {
+	
+	class beatmap_player : public ui_component{
+		beatmap b_;
+		sf::Time notes_visible_duration_;
+		event_handlers<beatmap &> handlers_;
+
+	private:
+		beatmap_player(beatmap && b);
+	public:
+		result_t event_procedure(const sf::Event & e) override;
+		void draw(sf::RenderTarget &, sf::RenderStates) const override;
+		sf::FloatRect get_rect() const noexcept override;
+
+	};
+}
